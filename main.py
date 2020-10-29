10d38c55eb6e716fe5d1622819f9e23e3b457699
TranNgocTinh0810 <55842203+TranNgocTinh0810@users.noreply.github.com>
2020-10-29 11:43:51 +0700

    Fix3.3 + Add 3.4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import operator
 from csv import reader
 import pandas as pd
 import numpy as np
@@ -9,6 +10,11 @@ def load_csv(filename):
     dataset = list(lines)
     return dataset
 def list_missing(dataset):
+    '''
+    Liệt kê các cột bị thiếu dữ liệu.
+    :param dataset: List
+    :return: List
+    '''
     list_missing_col=[]
     for col in range(0,len(dataset[0]),1):
         for row in range(0,len(dataset),1):
@@ -18,6 +24,11 @@ def list_missing(dataset):
     print("Cac Cot Bi Thieu Du Lieu La : ",list_missing_col)
     return list_missing_col
 def count_missing_col(dataset):
+    '''
+    Đếm số dòng bị thiếu dữ liệu.
+    :param dataset: list
+    :return: int
+    '''
     count=0
     for row in range(0,len(dataset),1):
         for col in range(0,len(dataset[0]),1):
@@ -41,6 +52,11 @@ def is_number(s):
                 return False
 
 def col_mean(dataset):
+    '''
+    Mode Col: if values of col are numeric
+    :param dataset: List
+    :return: float
+    '''
     sum=0
     length=0
     for i in dataset:
@@ -49,6 +65,11 @@ def col_mean(dataset):
             length+=1
     return sum / length
 def col_mode(dataset):
+    '''
+    Mode Col: if values of col are categorical
+    :param dataset: list
+    :return: string
+    '''
     max=0
     result=''
     for i in dataset:
@@ -57,6 +78,13 @@ def col_mode(dataset):
             result=i
     return result
 def fill_missing(dataset):
+    '''
+    Điền giá trị bị thiếu bằng phương pháp mean, median (cho thuộc tính numeric) và mode
+    (cho thuộc tính categorical). Lưu ý: khi tính mean, median hay mode các bạn bỏ qua giá
+    trị bị thiếu.
+    :param dataset:
+    :return:
+    '''
     print("dt1",dataset.__len__())
     SubDataset=[]  #Check Str or Int,Float
     count1=0
@@ -81,20 +109,66 @@ def fill_missing(dataset):
         i=i+1
     print("dt3",dataset.__len__())
     return dataset
-def run(i,dataset):
+
+def del_row_scale(dataset,scale):
+    '''
+    Xóa các dòng bị thiếu dữ liệu với ngưỡng tỉ lệ thiếu cho trước (Ví dụ: xóa các dòng bị
+    thiếu hơn 50% giá trị các thuộc tính)
+    :param dataset: list
+    :param scale: int.example 50-> meaning 50 percent
+    :return: List
+    '''
+    scale=int(scale)
+    list_missing_row = {}
+    for row in range(0, len(dataset), 1):
+        for col in range(0, len(dataset[0]), 1):
+            if (dataset[row][col] == ''):
+                if(row not in list_missing_row):
+                    list_missing_row[row]=1
+                else:
+                    list_missing_row[row]+=1
+    print(list_missing_row)
+    #value=list(list_missing_row.values())
+    #print(max(value))
+    lengthrow=dataset[0].__len__()
+    for row in list(list_missing_row.keys()):
+        if((list_missing_row[row]/lengthrow)<=(scale/100)):
+            del list_missing_row[row]
+
+    check=0
+    for row in list(list_missing_row.keys()):
+        dataset.pop(row-check)
+        check=check+1
+
+    print(list_missing_row)
+    print(list_missing_row.__len__())
+    print(dataset.__len__())
+    print(dataset)
+    return dataset
+
+def run_2_parameter(i,dataset):
     if(i=='list-missing'):
         list_missing(dataset)
     if(i=='count_missing_col'):
         count_missing_col(dataset)
     if(i=='fill_missing'):
         dataset2=fill_missing(dataset)
-        with open('Cau 3.3.csv', 'w') as f:
-            write = csv.writer(f,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            print("dt2",dataset2.__len__())
-            write.writerows(dataset2)
+        df = pd.DataFrame(dataset2)
+        df.to_csv("Cau3_3.csv")
+    i
+def run_3_parameter(x,y,dataset):
+    if(x=='del_row_scale'):
+        dataset=del_row_scale(dataset,y)
+        df=pd.DataFrame(dataset)
+        df.to_csv("Cau3_4.csv")
 
 
-dataset=load_csv(sys.argv[2])
-run(sys.argv[1],dataset)
+
+if(sys.argv.__len__()==3):
+    dataset = load_csv(sys.argv[2])
+    run_2_parameter(sys.argv[1],dataset)
+elif (sys.argv.__len__()==4):
+    dataset = load_csv(sys.argv[3])
+    run_3_parameter(sys.argv[1],sys.argv[2],dataset)
 
 
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 # Load a CSV file
+import csv
 from csv import reader
 def load_csv (filename):
     file=open(filename,"r")
@@ -6,17 +7,37 @@ def load_csv (filename):
     dataset=list(lines)
     return dataset
 def is_number(s):
-    if(s!='')
+    if(s!=''):
         try:
             float(s)
             return True
         except ValueError:
             return False
 # Load dataset
-filename = 'house-prices.csv'
+'''filename = 'house-prices.csv'
 dataset = load_csv(filename)
 #print ('Loaded data file{0} with {1} rows and {2} columns'.format(filename,len(dataset), len(dataset[0])))
-print(dataset[0][0])
+print(dataset[0][0])'''
+data=[[123],[234],[345]]
+with open('Cau 3.1.csv', 'w') as f:
+    write = csv.writer(f)
+    print("dt2", data)
+    write.writerows(data)
+    f.close()
+a=load_csv('Cau 3.1.csv')
+print(a.__len__(),data.__len__())
 
+import pandas as pd
 
+# list of name, degree, score
+nme = ["aparna", "pankaj", "sudhir", "Geeku"]
+deg = ["MBA", "BCA", "M.Tech", "MBA"]
+scr = [90, 40, 80, 98]
 
+# dictionary of lists
+dict = {'name': nme, 'degree': deg, 'score': scr}
+
+df = pd.DataFrame(dict)
+
+# saving the dataframe
+df.to_csv('GFG.csv')
